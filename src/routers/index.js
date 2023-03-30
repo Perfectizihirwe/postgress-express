9c18b8f03927ee9f601cc70521a88b09b4a04ac1
Perfectizihirwe <perfectizihirwe@gmail.com>
2023-03-30 14:43:50 -0700

    Adding postgress

---
+++ src/routers/index.js
@@ -0,0 +1,1 @@
+export * from "./newsRouter.js";
---
+++ src/utils/database.js
@@ -0,0 +1,5 @@
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize(
+  "postgres://rmaulbca:xOctphXcmZeiyRjC37ZTmmbuNVFurQYc@babar.db.elephantsql.com/rmaulbca"
+);
